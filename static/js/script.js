985d9911b4ffa9e6216086c7fe14a23adb236e1f
Rexayo <abbaabbarex@yahoo.com>
2021-01-25 17:03:27 +0000

    Added video gallery layout

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,15 @@ $(document).ready(function () {
     $('.sidenav').sidenav({ edge: "right" });
      $('.modal').modal();
      $('#modal1').modal('open');
+    //  $('.first-video').hover(
+    //      function() {
+    //          $('.second-video').css('box-shadow', '-100px 0 30px #000');
+    //      },function() {
+    //          $('.second-video').css('box-shadow', '');
+    //      }
+    //  )
+
+
 });
 
 
